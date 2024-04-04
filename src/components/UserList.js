272a376b9607656a2ed92b7dfbd0e0e3d6593c94
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-04-04 21:35:16 +0530

    search input

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -8,6 +8,8 @@ const UserList = () => {
     const [users, setUsers] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [usersPerPage] = useState(10);
+    const [searchQuery, setSearchQuery] = useState();
+    const [filterData, setFilterData] = useState(user);
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -23,6 +25,19 @@ const UserList = () => {
         fetchUsers();
     }, []);
 
+    const handleItemSearch = (e) => {
+        const query = e.target.value;
+        setSearchQuery(query);
+        filteredData(query);
+    };
+
+    const filteredData = (query) => {
+        const filtered = filterData.filter(item =>
+            item.toLowerCase().includes(query.toLowerCase())
+        );
+        setFilterData(filtered);
+    };
+
     // Get current users
     const indexOfLastUser = currentPage * usersPerPage;
     const indexOfFirstUser = indexOfLastUser - usersPerPage;
@@ -32,18 +47,30 @@ const UserList = () => {
     const paginate = pageNumber => setCurrentPage(pageNumber);
 
     return (
-        <div className="container mx-auto mt-8">
-            <div className="grid grid-cols-2 gap-4">
-                {currentUsers.map(user => (
-                    <ContactCard key={user.id} user={user} />
-                ))}
+        <>
+
+            <div className="container mx-auto mt-8">
+                <input
+                    type='text'
+                    value={searchQuery}
+                    onChange={handleItemSearch}
+                    setFilterData={filterData}
+                    width="100%"
+                    height="50vh"
+                />
+                <div className="grid grid-cols-2 gap-4">
+                    {currentUsers.map(user => (
+                        <ContactCard key={user.id} user={user} />
+                    ))}
+                </div>
+                <Pagination
+                    usersPerPage={usersPerPage}
+                    totalUsers={users.length}
+                    paginate={paginate}
+                />
             </div>
-            <Pagination
-                usersPerPage={usersPerPage}
-                totalUsers={users.length}
-                paginate={paginate}
-            />
-        </div>
+        </>
+
     );
 };
 
